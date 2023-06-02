5653b8efe9de8f30aaa1cdb686251a8d5a84d271
sandamalt <sandamal.live@gmail.com>
2023-06-03 01:39:18 +0530

    Payment Gateway adding Setup cpmplted

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,27 +1,59 @@
 import createError from "../utils/createError.js";
 import Order from "../models/order.model.js";
 import Gig from "../models/gig.model.js";
+import Stripe from "stripe";
 
-export const createOrder = async (req, res, next) => {
-  try {
-    const gig = await Gig.findById(req.params.gigId);
+export const intent = async (req, res, next) => {
+  const stripe = new Stripe(process.env.STRIPE);
 
-    const newOrder = new Order({
-      gigId: gig._id,
-      img: gig.cover,
-      title: gig.title,
-      buyerId: req.userId,
-      sellerId: gig.userId,
-      price: gig.price,
-      payment_intent: "temporary",
-    });
+  const gig = await Gig.findById(req.params.id);
 
-    await newOrder.save();
-    res.status(200).send("Successful");
-  } catch (err) {
-    next(err);
-  }
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  const newOrder = new Order({
+    gigId: gig._id,
+    img: gig.cover,
+    title: gig.title,
+    buyerId: req.userId,
+    sellerId: gig.userId,
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
+
+  await newOrder.save();
+
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
 };
+
+// export const createOrder = async (req, res, next) => {
+//   try {
+//     const gig = await Gig.findById(req.params.gigId);
+
+//     const newOrder = new Order({
+//       gigId: gig._id,
+//       img: gig.cover,
+//       title: gig.title,
+//       buyerId: req.userId,
+//       sellerId: gig.userId,
+//       price: gig.price,
+//       payment_intent: "temporary",
+//     });
+
+//     await newOrder.save();
+//     res.status(200).send("Successful");
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 export const getOrders = async (req, res, next) => {
   try {
